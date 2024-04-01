b0c69f119775bb124bd6c9306c5877c3adb9f3a5
Mao Chenxu <maochenx@teach.cs.toronto.edu>
2024-04-01 15:50:19 -0400

    debugged q2.sql

--- q2.sql
+++ q2.sql
@@ -3,9 +3,12 @@ SET SEARCH_PATH TO Conference, public;
 DROP TABLE IF EXISTS q2 CASCADE;
 CREATE TABLE q2 (
     person_id INT NOT NULL,
-    conference_count INT NOT NULL  
+    conference_count INT NOT NULL,
+
+    PRIMARY KEY (person_id, conference_count)
 );
 
 SELECT DISTINCT person_id, COUNT(DISTINCT conf_id) AS conference_count
 INTO q2
-FROM Attends;
+FROM Attends
+GROUP BY person_id;
